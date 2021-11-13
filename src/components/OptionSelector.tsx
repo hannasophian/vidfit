6b42e670fa61b6b1e55ecd6efe42f5acf3902de8
Hanna <sophianhanna@gmail.com>
2021-11-13 19:48:26 +0000

    Format

--- src/components/OptionSelector.tsx
+++ src/components/OptionSelector.tsx
@@ -5,19 +5,21 @@ export default function OptionSelector(): JSX.Element {
   const [tags, setTags] = useState<string[]>([]);
   const times = [10, 15, 20, 30, 45, 60];
   const timeOptions = times.map((time) => (
-    <option key={time} value={time}>{time} mins</option>
+    <option key={time} value={time}>
+      {time} mins
+    </option>
   ));
   const [duration, setDuration] = useState<number>(times[0]);
 
   const handleClickCheckbox = (newTag: string) => {
     if (tags.includes(newTag)) {
-      let currentTags = [...tags]
+      let currentTags = [...tags];
       currentTags.splice(tags.indexOf(newTag), 1);
       setTags(currentTags);
     } else {
-      setTags([...tags, newTag])
+      setTags([...tags, newTag]);
     }
-  }
+  };
 
   const bodyPartOptions = [
     "Full body",
@@ -28,9 +30,14 @@ export default function OptionSelector(): JSX.Element {
     "Back",
   ].map((part) => (
     <div>
-      <input type="checkbox" onChange={() => handleClickCheckbox(part)}
-      // onChange={(event) => !tags.includes(event.target.value)? setTags([...tags, part]):setTags(tags.splice(tags.indexOf(event.target.value),1))}
-      id={part} name="bodypart" value="coding" />
+      <input
+        type="checkbox"
+        onChange={() => handleClickCheckbox(part)}
+        // onChange={(event) => !tags.includes(event.target.value)? setTags([...tags, part]):setTags(tags.splice(tags.indexOf(event.target.value),1))}
+        id={part}
+        name="bodypart"
+        value="coding"
+      />
       <label>{part}</label>
     </div>
   ));
@@ -44,16 +51,27 @@ export default function OptionSelector(): JSX.Element {
     "dance",
   ].map((type) => (
     <div>
-      <input type="checkbox" onChange={() => handleClickCheckbox(type)} id={type} name="workouttype" value="coding" />
+      <input
+        type="checkbox"
+        onChange={() => handleClickCheckbox(type)}
+        id={type}
+        name="workouttype"
+        value="coding"
+      />
       <label>{type}</label>
     </div>
   ));
   return (
     <>
       <label id="timeselector">Select workout time: </label>
-      <select id="timeselector" value = {duration} onChange={(e) => setDuration(parseInt(e.target.value))}>{timeOptions}</select>
-      <MaxCheckMessage
-        duration={duration} />
+      <select
+        id="timeselector"
+        value={duration}
+        onChange={(e) => setDuration(parseInt(e.target.value))}
+      >
+        {timeOptions}
+      </select>
+      <MaxCheckMessage duration={duration} />
       <fieldset>
         <legend>Choose by body part</legend>
         {bodyPartOptions}
@@ -61,8 +79,9 @@ export default function OptionSelector(): JSX.Element {
         {workoutTypeOptions}
       </fieldset>
 
-      <button onClick={() => console.log(duration, tags)}>Give me some vids!</button>
+      <button onClick={() => console.log(duration, tags)}>
+        Give me some vids!
+      </button>
     </>
   );
 }
-
